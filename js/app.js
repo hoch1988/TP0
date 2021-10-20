9e43d23830a7a7f9bc53a5402ec68ac667ad093d
hoch1988 <hoch1988@hotmail.com>
2021-10-20 20:12:03 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let myform = document.getElementsByTagName("form")
+
+console.dyr(myform);
+
+myform[0].addEventListener("submit",params)
+params.preventdefault();    
+console.log ("enviando data...")
+
+
+let myinput = document.getElementsByTagNameNS("input")
+console.log(myinput[0])
+
+let myinputstring = myinput[0].value
+if(myinputstring.lengh < 8 ){
+    console.log(myinput[0])
+params.preventdefault();
+console.error("usuario con pocos caracteres...")
+myinput[0].classlist.add('style...')
+}else {
+    console.log("enviando data...")
+}
+}
+};
